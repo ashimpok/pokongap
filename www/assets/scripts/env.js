650e4835ccaa3caf2af388331f9d33cdf8566662
Ashim Pokharel <ashimpok@gmail.com>
2012-03-09 20:04:28 -0500

    added user name

--- www/assets/scripts/env.js
+++ www/assets/scripts/env.js
@@ -1,7 +1,7 @@
-//Ashim. This script contains enviroment constants used throughout the site
+//ashimpok. This script contains enviroment constants used throughout the site
 var ServerUrl = "http://localhost/pub/MobileRequestHandler.ashx";
 
-//Ashim. Pages must call this to setup cross domain access
+//ashimpok. Pages must call this to setup cross domain access
 function pageSetup() {
     $.support.cors = true;
     $.mobile.allowCrossDomainPages = true;    
