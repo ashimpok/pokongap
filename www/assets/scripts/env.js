d0a7901152d10cfe8b628321d7d3c7de81f068ca
Ashim Pokharel <ashimpok@gmail.com>
2012-03-09 19:49:02 -0500

    added www directory for PhoneGap development

---
+++ www/assets/scripts/env.js
@@ -0,0 +1,8 @@
+﻿//Ashim. This script contains enviroment constants used throughout the site
+var ServerUrl = "http://localhost/CISIPortalWeb/pub/MobileRequestHandler.ashx";
+
+//Ashim. Pages must call this to setup cross domain access
+function pageSetup() {
+    $.support.cors = true;
+    $.mobile.allowCrossDomainPages = true;    
+}
