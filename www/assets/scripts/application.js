5e35be00f2ce10904691ba0273470f80b964f779
Ashim Pokharel <ashimpok@gmail.com>
2012-03-21 22:53:32 -0400

    Testing asp.net ashx handler with json

--- www/assets/scripts/application.js
+++ www/assets/scripts/application.js
@@ -1,5 +1,16 @@
 //Author: ashimpok@gmail.com
 //License: UYP - Use as you please!
 function ApplicationShowError(title, message) {
-    alert(title + '\n' + message);
+  alert(title + '\n' + message);
+}
+
+function ApplicationShowMessage(title, message) {
+  alert(title + '\n' + message);
+}
+
+/*Request objects*/
+function MobileLoginRequest() {
+  this.RequestType = "MobileLoginRequest"
+  this.UserName = null
+  this.Password = null
 }
--- www/assets/scripts/env.js
+++ www/assets/scripts/env.js
@@ -5,28 +5,42 @@
 //used throughout the site
 
 var Environment = {
-    JsonHandler: "http://localhost/pub/MobileRequestHandler.ashx",
-    getInitMethod: function () {
-        //return "mobileinit"
-        return "ready";
-    },
-
-    sendRequest: function (data) {
+    JsonHandler: "http://localhost:51045/WebSite1/MobileInterface.ashx",
+    InitName: "ready", //"mobileinit"
+    
+    PostRequest: function (jsonObject, onSuccess, onError) {
+        console.log(JSON.stringify(jsonObject));
         
+        $.ajax({
+          type: "GET", /*MUST USE GET*/
+          url: this.JsonHandler,
+          cache: false,
+          dataType: "json",
+          data: { jsonData: JSON.stringify(jsonObject) },
+          contentType: "application/json; charset=utf-8",          
+          success: function(data, Status){                   
+            if(typeof(onSuccess) === "function")
+              onSuccess(data, Status);
+          },
+          error: function(data, Status, err){
+            if(typeof(onError) === "function")
+              onError(data, Status, err);
+          }
+        });      
     },
 
     //Pages must call this to setup cross domain access
     //and other global settings
-    pageSetup: function () {
+    PageSetup: function () {
         $.support.cors = true;
         $.mobile.allowCrossDomainPages = true;
-
+        
         //If there are date-birth fields attach datetimepicker
         $('.date-birth').datepicker(
             {
                 changeYear: true,
                 changeMonth: true,
-                yearRange: "c-60:c-14"
+                yearRange: "c-60:c-14"                
             }
         );
     }
