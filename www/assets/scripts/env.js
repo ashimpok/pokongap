68ef62c9216339e5b225a50dc472f6ba05b76fe6
Ashim Pokharel <ashimpok@gmail.com>
2012-03-27 19:58:21 -0400

    continous work

--- www/assets/scripts/env.js
+++ www/assets/scripts/env.js
@@ -5,7 +5,8 @@
 //used throughout the site
 
 var Environment = {
-    JsonHandler: "http://localhost:51045/WebSite1/MobileInterface.ashx",
+    JsonHandler: "http://127.0.0.1:8080/MobileHandler.ashx", 
+    				//"http://localhost:51045/WebSite1/MobileInterface.ashx",
     InitName: "pageinit", //"pageinit" or "ready"
     
     PostRequest: function (jsonObject, onSuccessCall, onErrorCall) {
